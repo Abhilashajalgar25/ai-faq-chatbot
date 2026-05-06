bdddf84d70d86ec68fdc8cd80e156bfce110f614
Abhilasha Jalgar <abhilashajalgar25@gmail.com>
2026-05-07 02:27:15 +0530

    Update script.js

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ async function sendMessage() {
   chatbox.innerHTML += `<p><b>You:</b> ${userMessage}</p>`;
 
   const response = await fetch(
-    "https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=AIzaSyCJdv612w6-zHlvu95Uo7754FgoMyBOMBo",
+    "https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=AIzaSyC_TT2RaaagxiwF1EcoHosxnN7oemhGd2k",
     {
       method: "POST",
       headers: {
