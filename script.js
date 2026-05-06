d8793171b793543cc73ed23bab8fc6bf778c6d2a
Abhilasha Jalgar <abhilashajalgar25@gmail.com>
2026-05-07 01:53:44 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+async function sendMessage() {
+
+  const input = document.getElementById("userInput");
+  const chatbox = document.getElementById("chatbox");
+
+  const userMessage = input.value;
+
+  chatbox.innerHTML += `<p><b>You:</b> ${userMessage}</p>`;
+
+  const response = await fetch(
+    "https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=AIzaSyCJdv612w6-zHlvu95Uo7754FgoMyBOMBo",
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        contents: [{
+          parts: [{ text: userMessage }]
+        }]
+      })
+    }
+  );
+
+  const data = await response.json();
+
+  const botReply =
+    data.candidates[0].content.parts[0].text;
+
+  chatbox.innerHTML += `<p><b>Bot:</b> ${botReply}</p>`;
+
+  input.value = "";
+}
